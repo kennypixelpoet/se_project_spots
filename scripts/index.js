df966ab7e4a8152c10b4d40bf5b0ea503f754eee
Kenny <kkeagan20@gmail.com>
2025-12-27 03:15:23 -0500

    Add opening and closing modal

---
+++ scripts/index.js
@@ -0,0 +1,42 @@
+console.log("JavaScript is connected")
+    //Edit Profile modal
+
+const editProfileBtn = document.querySelector(".profile__edit-btn")
+const editProfileModal = document.querySelector("#edit-profile-modal")
+const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn")
+
+editProfileBtn.addEventListener("click", function() {
+    editProfileModal.classList.add("modal_is-opened")
+})
+
+editProfileCloseBtn.addEventListener("click", function() {
+    editProfileModal.classList.remove("modal_is-opened")
+})
+
+const editProfileForm = editProfileModal.querySelector(".modal__form")
+editProfileForm.addEventListener("submit", function(event) {
+    event.preventDefault()
+    editProfileModal.classList.remove("modal_is-opened")
+        console.log("Edit Profile form submitted")
+})
+
+    //New Post modal
+
+const newPostBtn = document.querySelector(".profile__add-btn")
+const newPostModal = document.querySelector("#new-post-modal")
+const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn")
+
+newPostBtn.addEventListener("click", function() {
+    newPostModal.classList.add("modal_is-opened")
+})
+
+newPostCloseBtn.addEventListener("click", function() {
+    newPostModal.classList.remove("modal_is-opened")
+})
+
+const newPostForm = newPostModal.querySelector(".modal__form")
+newPostForm.addEventListener("submit", function(event) {
+    event.preventDefault()
+    newPostModal.classList.remove("modal_is-opened")
+    console.log("New Post form submitted")
+})
